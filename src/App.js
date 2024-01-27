439bcd53685d48acffecdc05d8105bd9e80e3370
Amber Younus <amberyounus22@gmail.com>
2024-01-27 01:41:53 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './App.css';
 import JSON_DATA from './JSON_DATA.json';
 
 function App() {
   return (
     <>
-      <div className=''>
+      <div className='App'>
         <input type='text' placeholder='Search...' />
         {JSON_DATA.map((val, key) => {
-          return <div>{val.first_name}</div>
+          return (
+          <div className='data' key={key}>
+            <p> {val.first_name}</p>
+          </div>
+          )
         })}
       </div>
     </>
