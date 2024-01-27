86c41bd20f77295e503a6137e95afff9ffca2d39
Amber Younus <amberyounus22@gmail.com>
2024-01-26 23:09:52 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import JSON_DATA from './JSON_DATA.json';
 
 function App() {
   return (
- <>
- </>
+    <>
+      <div className=''>
+        <input type='text' placeholder='Search...' />
+        {JSON_DATA.map((val, key) => {
+          return <div>{val.first_name}</div>
+        })}
+      </div>
+    </>
   );
 }
 
