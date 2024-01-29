2d5b235d974fd40ec34acbf05f255458b01d1860
Amber Younus <amberyounus22@gmail.com>
2024-01-29 00:26:53 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -4,30 +4,29 @@ import JSON_DATA from "./JSON_DATA.json";
 
 function App() {
   const [search, setSearch] = useState("");
+
   return (
-    <>
-      <div className="App">
-        <input
-          type="text"
-          placeholder="Search..."
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-        />
-        {JSON_DATA.map((val, key) => {
-          return (
-            <div className="data" key={key}>
-              <p>{val.id}</p>
-              <p> {val.first_name}</p>
-              <p>{val.last_name}</p>
-              <p>{val.email}</p>
-              <p>{val.gender}</p>
-              <p>{val.ip_address}</p>             
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      {JSON_DATA.map((val, key) => {
+        return (
+          <div className="data" key={key}>
+            <p>{val.id}</p>
+            <p> {val.first_name}</p>
+            <p>{val.last_name}</p>
+            <p>{val.email}</p>
+            <p>{val.gender}</p>
+            <p>{val.ip_address}</p>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
