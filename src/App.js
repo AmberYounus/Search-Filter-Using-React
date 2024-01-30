accc2b03ef37d5b58c144e72f14e5e23508acb18
Amber Younus <amberyounus22@gmail.com>
2024-01-30 01:56:38 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Search Filter</h1>
       <input
         type="text"
         placeholder="Search..."
@@ -14,15 +15,23 @@ function App() {
           setSearch(e.target.value);
         }}
       />
-      {JSON_DATA.map((val, key) => {
+      {JSON_DATA.filter((val) => {
+        if (search == "") {
+          return val;
+        } else if (
+          val.first_name.toLowerCase().includes(search.toLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val, key) => {
         return (
           <div className="data" key={key}>
-            <p>{val.id}</p>
-            <p> {val.first_name}</p>
-            <p>{val.last_name}</p>
-            <p>{val.email}</p>
+            {/* <p>{val.id}</p> */}
+            <p> {val.first_name}{val.last_name}</p><p></p>
+            
+            {/* <p>{val.email}</p>
             <p>{val.gender}</p>
-            <p>{val.ip_address}</p>
+            <p>{val.ip_address}</p> */}
           </div>
         );
       })}
